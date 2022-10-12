63cde5d16c8fb6f1bcd4e3e0cd36ff64929da0a7
Namae <namaeconde@gmail.com>
2022-10-12 21:49:26 +0800

    Update main background color

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   AppTheme._();
 
-  static const Color backgroundColor = Color(0xFFB2DEF8);
+  static const Color backgroundColor = Color(0xFFE1F5FE);
   static const Color buttonColor = Color(0xFF2FD1C5);
   static const Color textColor = Color(0xFF00394C);
   static const Color shadowColor = Color(0xFF3A5160);
